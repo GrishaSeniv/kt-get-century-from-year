fe16ac0988bbc284a9ee21870b3ae4ffbdcc625b
Hryhorii Seniv <hryhorii.seniv@javatar.pro>
2025-12-26 11:10:46 +0200

    Implemented task

--- src/main/kotlin/mate/academy/GetCentury.kt
+++ src/main/kotlin/mate/academy/GetCentury.kt
@@ -1,5 +1,15 @@
 package mate.academy
 
-fun getCentury(year: Int) : Int {
-    return 0
+fun getCentury(year: Int): Int {
+    if (year <= 0) {
+        return 0
+    }
+
+    val result = year / 100
+
+    if (year % 100 == 0) {
+        return result
+    }
+
+    return result + 1
 }
